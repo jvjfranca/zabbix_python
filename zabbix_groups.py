d665b3d4b9f57159a1d094814fab342dbfd649d4
Jose Vicente <jvjfranca@gmail.com>
2016-06-10 09:23:18 -0300

    Adding comments and Printing

--- zabbix_groups.py
+++ zabbix_groups.py
@@ -1,15 +1,25 @@
+# Script Name	: zabbix_groups.py
+# Author		: Jose Vicente Jonas Franca
+# Created		: 10 Jun 2016
+# Description   : Create Users Groups in Zabbix reading a txt file using the Zabbix API
+
+# import zabbix_api
 from zabbix_api import ZabbixAPI
 
+# set the zabbix server
 zapi = ZabbixAPI(server="http://192.168.1.80/zabbix")
 
+# login into zabbix server using username and password
 zapi.login("user","password")
 
-with open('grupos.txt', 'r')as clientes:
-    c = clientes.readlines()
-    for row in c:
-        lista = (row.strip('\n')).split(',')
+# read the lines from grupos.txt then create the User Group with the name in the line
+with open('grupos.txt', 'r')as clientes: # open the file as clientes
+    c = clientes.readlines() # read the lines from the file
+    for row in c: # for each row in c
+        lista = (row.strip('\n')).split(',') # strip the new lines and split the comma
         try:
-            group_name = str(lista[0])
-            zapi.usergroup.create({"name": group_name})
+            group_name = str(lista[0]) # get the group name
+            zapi.usergroup.create({"name": group_name}) # create the user group name
+            print("[+] User Group {} successfully created".format(group_name))
         except:
-            print("Não foi possivel adicionar o Grupo")
+            print("[-] It was not possible to create the group")
